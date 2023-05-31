0affafa9ff427f54c1de536d38e73d1b45d58f2d
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-05-31 19:10:32 +0200

    create new branch for test props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,18 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1];
+const PokemonCard = (props) => {
+    console.log (props)
     
-    return ((pokemon.imgSrc)?
+    return ((props.imgSrc)?
             <figure>
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
-                <figcaption> {pokemon.name} </figcaption>
+                <img src={props.imgSrc} alt={props.name} />
+                <figcaption> {props.name} </figcaption>
             </figure>
     :    
             <figure>
                 <p>???</p>
-                <figcaption> {pokemon.name} </figcaption>
+                <figcaption> {props.name} </figcaption>
             </figure>
     )
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 export default PokemonCard
