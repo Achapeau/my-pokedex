d1955e1ae0c5c7ec1f980f13769094af99204f51
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-06-04 12:13:46 +0200

    create alert and pika envents

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,48 @@ import { useState } from 'react';
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 import NavBar from './components/NavBar';
+import { useEffect } from 'react';
+
+
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+    name: "bulbasaur",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   
-  const [pokemonIndex, setPokemonIndex] = useState(3);
+  useEffect(() => alert('hello pokemon trainer :)'), []);
+  
+  const [pokemonIndex, setPokemonIndex] = useState(0);
   const pokemon = pokemonList[pokemonIndex];
 
   return (
     <>
       <div>
         <PokemonCard imgSrc={pokemon.imgSrc} name={pokemon.name}/>
-        <NavBar pokemonIndex={pokemonIndex} length={pokemonList.length} setPokemonIndex={setPokemonIndex}/>
+        <NavBar pokemon={pokemon.name} pokemonIndex={pokemonIndex} length={pokemonList.length} setPokemonIndex={setPokemonIndex}/>
       </div>
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,30 @@
-const NavBar = ({pokemonIndex, length, setPokemonIndex}) => {
+
+
+const NavBar = ({pokemonIndex, length, setPokemonIndex, pokemon}) => {
     
-    const handleClickBefore = () => {
-        setPokemonIndex(pokemonIndex - 1);
-    }
     
-    const handleClickAfter = () => {
-        setPokemonIndex(pokemonIndex + 1);
-    }
+        
+        const handleClickBefore = () => {
+            setPokemonIndex(pokemonIndex - 1);
+        }
+        
+        const handleClickAfter = () => {
+            setPokemonIndex(pokemonIndex + 1);
+        }
+        
     return (
-    <>
-        {pokemonIndex > 0 && <button onClick={handleClickBefore}>Before</button>}
-        {pokemonIndex < length - 1 && <button onClick={handleClickAfter}>After</button>}
-    </>
+        (pokemon === "pikachu")?
+            (alert('pika pikachu!!!'),
+            <>
+                {pokemonIndex > 0 && <button onClick={handleClickBefore}>Before</button>}
+                {pokemonIndex < length - 1 && <button onClick={handleClickAfter}>After</button>}
+            </>):
+        (
+            <>
+                {pokemonIndex > 0 && <button onClick={handleClickBefore}>Before</button>}
+                {pokemonIndex < length - 1 && <button onClick={handleClickAfter}>After</button>}
+            </>
+        )
     )
 }
     
